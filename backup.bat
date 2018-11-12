2dcec8c4c0298e42a2634ff4b9b5057caa35d8c3
MDipper <MDipper@users.noreply.github.com>
2018-11-12 16:23:37 +0800

    Add files via upload

---
+++ backup.bat
@@ -0,0 +1,3 @@
+@echo on
+robocopy    D:\×ÊÁÏ\    \\192.168.1.44\backup	  /E   /XO
+pause
---
+++ pbaidu.bat
@@ -0,0 +1,4 @@
+@echo off :
+date/t >>E:\baidu.txt  
+time/t >>E:\baidu.txt 
+ping -n 3600000 14.215.177.39  >>E:\baidu.txt  
